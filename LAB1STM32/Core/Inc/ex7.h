de30beae8cef4bcb1a97045756b3824a9a459199
ADMIN-PC\Admin <dao.doandao1603@hcmut.com>
2024-09-18 11:17:50 +0700

    ex7

---
+++ LAB1STM32/Core/Inc/ex7.h
@@ -0,0 +1,13 @@
+/*
+ * ex7.h
+ *
+ *  Created on: Sep 18, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EX7_H_
+#define INC_EX7_H_
+
+
+
+#endif /* INC_EX7_H_ */
--- LAB1STM32/Core/Inc/main.h
+++ LAB1STM32/Core/Inc/main.h
@@ -32,6 +32,11 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "ex1.h"
+#include "ex2.h"
+#include "ex3.h"
+#include "ex4.h"
+#include "ex5.h"
 
 /* USER CODE END Includes */
 
