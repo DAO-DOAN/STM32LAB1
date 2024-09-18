8e49895b5816dc3d4ed32d0a05a09df82c22df5b
ADMIN-PC\Admin <dao.doandao1603@hcmut.com>
2024-09-18 10:21:49 +0700

    Header and source ex2

---
+++ LAB1STM32/Core/Inc/ex2.h
@@ -0,0 +1,13 @@
+/*
+ * ex2.h
+ *
+ *  Created on: Sep 18, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EX2_H_
+#define INC_EX2_H_
+
+
+
+#endif /* INC_EX2_H_ */
---
+++ LAB1STM32/Core/Src/ex2.c
@@ -0,0 +1,8 @@
+/*
+ * ex2.c
+ *
+ *  Created on: Sep 18, 2024
+ *      Author: Admin
+ */
+
+
