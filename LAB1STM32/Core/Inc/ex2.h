bf667c4afec2f4e2540bb31f96807d4c19f323c5
ADMIN-PC\Admin <dao.doandao1603@hcmut.com>
2024-09-18 10:23:29 +0700

    Header and source ex2

--- LAB1STM32/Core/Inc/ex2.h
+++ LAB1STM32/Core/Inc/ex2.h
@@ -8,6 +8,16 @@
 #ifndef INC_EX2_H_
 #define INC_EX2_H_
 
-
+#include"main.h"
+int count;
+int status;
+void test_ex2();
+void red_on();
+void yellow_on();
+void green_on();
+void red_off();
+void yellow_off();
+void green_off();
+void run_ex2();
 
 #endif /* INC_EX2_H_ */
--- LAB1STM32/Core/Src/ex2.c
+++ LAB1STM32/Core/Src/ex2.c
@@ -6,3 +6,66 @@
  */
 
 
+#include "ex2.h"
+
+int status=RED_status;
+int count=0;
+void test_ex2(){
+	HAL_GPIO_TogglePin(LED_RED_GPIO_Port,LED_RED_Pin);
+	HAL_GPIO_TogglePin(LED_YELLOW_GPIO_Port,LED_YELLOW_Pin);
+	HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin);
+}
+void red_off(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port,LED_RED_Pin,SET);
+}
+void yellow_off(){
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port,LED_YELLOW_Pin,SET);
+}
+void green_off(){
+	HAL_GPIO_WritePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin,SET);
+}
+void red_on(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port,LED_RED_Pin,RESET);
+}
+void yellow_on(){
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port,LED_YELLOW_Pin,RESET);
+}
+void green_on(){
+	HAL_GPIO_WritePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin,RESET);
+}
+void run_ex2(){
+	  switch (status) {
+		case RED_status:
+			yellow_off();
+			green_off();
+			red_on();
+			count++;
+			if(count>=5){
+				status=YELLOW_status;
+				count=0;
+			}
+			break;
+		case YELLOW_status:
+			red_off();
+			green_off();
+			yellow_on();
+			count++;
+			if(count>=2){
+				status=GREEN_status;
+				count=0;
+			}
+			break;
+		case GREEN_status:
+			red_off();
+			yellow_off();
+			green_on();
+			count++;
+			if(count>=3){
+				status=RED_status;
+				count=0;
+			}
+			break;
+		default:
+			break;
+	}
+}
